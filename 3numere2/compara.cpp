fda31ce343b67cc403798f0a65ad740ec9b9397c
Cornea Adrian Emilian <emilian.cornea@yahoo.com>
2021-09-18 15:31:56 +0300

    safety check

--- 3numere2/compara.cpp
+++ 3numere2/compara.cpp
@@ -2,8 +2,12 @@
 
 void printTrei(int *numere, int dimensiune)
 {
-	std::sort(numere, numere+dimensiune);
-	std::cout << numere[0] << " " << numere[1] << " " << numere[2] << std::endl;
-	//pentru cele mai mari 3 numere
-	std::cout << numere[dimensiune-1] << " " << numere[dimensiune-2] << " " << numere[dimensiune-3] << std::endl;
+	if (dimensiune >= 3) {
+		std::sort(numere, numere + dimensiune);
+		std::cout << numere[0] << " " << numere[1] << " " << numere[2] << std::endl;
+		//pentru cele mai mari 3 numere
+		std::cout << numere[dimensiune - 1] << " " << numere[dimensiune - 2] << " " << numere[dimensiune - 3] << std::endl;
+	}
+	else
+		std::cout << "Prea putine elemente\n";
 }
